a2fe278cf60726185374fecaf0253ac8c6f5a046
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-05-11 17:52:50 +0530

    gretest number among 3 numbers

--- Program.java
+++ Program.java
@@ -1,21 +1,17 @@
-//calculate power of number
+//greatest number among 3 numbers.
 
 import java.util.Scanner;
-
 class Program{
     public static void main(String[] args) {
-        System.out.print("Enter the number: ");
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc  = new Scanner(System.in);
+        
+        System.out.print("Enter the 3 numbes: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        int number = scanner.nextInt();
-        System.out.print("Enter the number: ");
-        int power = scanner.nextInt();
+        int max = (a > b) ? (a > c ? a : c) :(b>c ? b: c);
 
-        int res = 1;
-        for (int i = 1; i <= power ; i++) {
-               res = res * number;
-        }
-
-        System.out.println("The power of "+number+" is: "+res);
+        System.out.println(" "+a+" "+b+" "+c+" Among these 3 numbers:"+max+" is maximum number");
     }
 }
