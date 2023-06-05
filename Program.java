6861e8af88651f10922bca7ea576d28839e75c50
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 13:06:09 +0530

    Increment/ Decrement Operator

--- Program.java
+++ Program.java
@@ -3,8 +3,9 @@ class Program
     public static void main(String args[])
     {
         int a=10, b=22;
-        System.out.println((a<b)&& (a>b));
-        System.out.println((a<b)|| (a>b));
-        System.out.println(!(a>b));
+        System.out.println(a++);    //Post Increment
+        System.out.println(++a);    //Pre Increment
+        System.out.println(a--);     //Post Decrement
+        System.out.println(--a);    //Pre Increment
     }
 }
