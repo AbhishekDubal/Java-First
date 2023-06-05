ae87c7ca779cc145cd045879d49a97990567bd41
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 23:57:30 +0530

    Nested for-loop

--- Program.java
+++ Program.java
@@ -4,10 +4,13 @@ class Program
     public static void main(String args[])
     {
        //Looping Statements (If you want to repeat certain line of code repeatedly)
-       //#3 for-loop
+       //#4 Nested for-loop
       
        for(int i = 1; i<=10; i++){
-        System.out.print(i+" ");
+            for(int j = 1; j<=10; j++){
+                System.out.print("*");
+            }
+            System.out.println();
        }
     }
 }
