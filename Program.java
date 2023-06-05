d0b0545d9ff86e5791c1cd563477ba94ac4a527f
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 12:50:40 +0530

    arithmatic operator

--- Program.java
+++ Program.java
@@ -1,18 +1,12 @@
-//print a table for given number.
-
-import java.util.Scanner;
-
-class Program {
-    public static void main(String[] args) {
-     Scanner sc = new  Scanner(System.in);
-
-     System.out.print("Enter the number of WHAT tabel you want to print: ");
-
-     int number  = sc.nextInt();
-
-     for(int i = 1; i <= 10; i++){
-        System.out.println(number+" * "+i+" = "+number*i);
-     }
-        
+class Program
+{
+    public static void main(String args[])
+    {
+        int a=10, b=22;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
     }
 }
