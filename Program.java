58dbf05bb4c3ab535ba6b57a3334bc93ecc03534
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 12:57:20 +0530

    Relational Operator

--- Program.java
+++ Program.java
@@ -3,10 +3,11 @@ class Program
     public static void main(String args[])
     {
         int a=10, b=22;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
+        System.out.println(a<b);
+        System.out.println(a>b);
+        System.out.println(a<=b);
+        System.out.println(a>=b);
+        System.out.println(a==b);
+        System.out.println(a!=b);
     }
 }
