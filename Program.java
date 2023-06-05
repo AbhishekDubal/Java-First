aff2a509a60faa8a0f2c77b26fc1faca1931f7de
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 23:49:52 +0530

    while loop usage

--- Program.java
+++ Program.java
@@ -1,30 +1,14 @@
-import java.util.Scanner;
+
 class Program
 {
     public static void main(String args[])
     {
-       //Decision Making Statements:(Conditional Statements)
-       Scanner sc =new Scanner(System.in);
-       System.out.print("ENter your choice: ");
-        char choice =sc.next().charAt(0);
-       
-        int a= 10, b=20;
-        switch(choice)
-        {
-            case '+' : System.out.println("Addition of a and b is = "+(a+b));
-            break;
-
-            case '-' : System.out.println("Subtraction of a and b is = "+(a-b));
-            break;
-
-            case  '*': System.out.println("Multiple of a and b is = "+(a*b));
-            break;
-
-            case  '/': System.out.println("Divison of a and b is = "+(a/b));
-            break;
-
-            default : System.out.println("Please choose valid operator");
+       //Looping Statements (If you want to repeat certain line of code repeatedly)
+       //#1 While loop
+        int num = 1;
+        while(num<=10){
+            System.out.println(num);//Printing natural numbers
+            num++;
         }
-        
     }
 }
