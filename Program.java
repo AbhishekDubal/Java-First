fe1305c4431801cb354e457fa03713771b29e848
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 16:20:08 +0530

    If and if else conditional statements

--- Program.java
+++ Program.java
@@ -1,16 +1,19 @@
+import java.util.Scanner;
 class Program
 {
     public static void main(String args[])
     {
-       //Ternary Operator(? :)#it works like if else conditions
-       int x=10, y=20;
-       int maximum = (x>y)? x:y;
-       System.out.println(maximum);
-
-       int num1 = 5, num2 =2, num3 = 4;
-    
-       int minimumNumber =  (num1<num2) ? (num1< num3 ? num1: num3) : (num2< num3 ? num2 : num3);
-
-       System.out.println(minimumNumber);
+       //Decision Making Statements:(Conditional Statements)
+       Scanner sc =new Scanner(System.in);
+       System.out.print("ENter your age: ");
+        int age =sc.nextInt();
+       if(age>=18)
+       {
+        System.out.println("Yes! you can Vote...!");
+       }
+       else
+       {
+        System.out.println("Sorry! Only 18 or more than that age group of people can Vote!");
+       }
     }
 }
