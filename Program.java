aa00703c7395838a565d5af4083c8cfb181a542f
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-05 13:00:58 +0530

    Logical Operator

--- Program.java
+++ Program.java
@@ -3,11 +3,8 @@ class Program
     public static void main(String args[])
     {
         int a=10, b=22;
-        System.out.println(a<b);
-        System.out.println(a>b);
-        System.out.println(a<=b);
-        System.out.println(a>=b);
-        System.out.println(a==b);
-        System.out.println(a!=b);
+        System.out.println((a<b)&& (a>b));
+        System.out.println((a<b)|| (a>b));
+        System.out.println(!(a>b));
     }
 }
