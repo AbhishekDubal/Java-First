99e40660956d680c8ad2f37206f17d606538d769
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 14:50:49 +0530

    calculate the power of given Number

--- Program.java
+++ Program.java
@@ -1,12 +1,22 @@
-
+import java.util.Scanner;
 class Program
 {
     public static void main(String args[])
     {
-      //print A to Z character 
+      //calculate the power of given Number:
+      Scanner sc= new Scanner(System.in);
 
-      for (char i = 'A'; i <= 'Z'; i++) {
-        System.out.print(i+" ");
+      System.out.print("Enter the Number: ");
+      int Number = sc.nextInt();
+      
+      System.out.print("ENter the POWER: ");
+      int POWER = sc.nextInt();
+
+      int result = 1;
+      
+      for (int i = 1; i <= POWER; i++) {
+        result = result * Number;
       }
+      System.out.print("The result of Number "+Number+" of power "+POWER+" is "+result);
     }
 }
