d38255509790d9a9217a6673837766993898a452
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 11:24:10 +0530

    Print odd or Even numbers according to the number given if it is odd or Even upto the range of the given number.

--- Program.java
+++ Program.java
@@ -1,16 +1,23 @@
-
+import java.util.Scanner;
 class Program
 {
     public static void main(String args[])
     {
-       //Looping Statements (If you want to repeat certain line of code repeatedly)
-       //#5 for-each loop(Mostly used Arrays cases.
-       // It'll help you to take the array elements one by one from one set of array and give it to other variable one by one).
+      //print all odd or even numbers between the Number that have been given.
       
-       int a[] = {10,12,14,145};
-       
-      for(int b:a){
-        System.out.print(b+" ");
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the Number: ");
+      int Number = sc.nextInt();
+
+      if(Number % 2 == 0){
+        for(int i = 0; i<= Number; i=i+2){
+          System.out.print(i+" ");
+        }
+      }
+      else{
+        for(int i=1; i<=Number; i=i+2){
+          System.out.print(i+" ");
+        }
       }
     }
 }
