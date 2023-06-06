c3555124415fba038c585056894e6706e31d6b2c
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 12:11:50 +0530

    The sum of all Odd and Even numbers of a given Number

--- Program.java
+++ Program.java
@@ -3,21 +3,26 @@ class Program
 {
     public static void main(String args[])
     {
-      //print all odd or even numbers between the Number that have been given.
+      //print SUM all odd or even numbers between the Number that have been given.
       
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter the Number: ");
       int Number = sc.nextInt();
 
+      int sum = 0 ;
       if(Number % 2 == 0){
         for(int i = 0; i<= Number; i=i+2){
-          System.out.print(i+" ");
+          // System.out.print(i+" ");
+          sum = sum+ i;
         }
+        System.out.println("The sum of all Even numbers of a given Number: "+sum);
       }
       else{
         for(int i=1; i<=Number; i=i+2){
-          System.out.print(i+" ");
+          // System.out.print(i+" ");
+          sum = sum + i;
         }
+        System.out.println("The sum of all odd numbers of a given Number: "+sum);
       }
     }
 }
