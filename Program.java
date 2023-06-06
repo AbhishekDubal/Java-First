1d51f821d41402133bbae09531b7044a03e8f473
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 12:56:20 +0530

    print character from user input

--- Program.java
+++ Program.java
@@ -3,26 +3,12 @@ class Program
 {
     public static void main(String args[])
     {
-      //print SUM all odd or even numbers between the Number that have been given.
-      
-      Scanner sc = new Scanner(System.in);
-      System.out.print("Enter the Number: ");
-      int Number = sc.nextInt();
+      //print character from user input
 
-      int sum = 0 ;
-      if(Number % 2 == 0){
-        for(int i = 0; i<= Number; i=i+2){
-          // System.out.print(i+" ");
-          sum = sum+ i;
-        }
-        System.out.println("The sum of all Even numbers of a given Number: "+sum);
-      }
-      else{
-        for(int i=1; i<=Number; i=i+2){
-          // System.out.print(i+" ");
-          sum = sum + i;
-        }
-        System.out.println("The sum of all odd numbers of a given Number: "+sum);
-      }
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the Character: ");
+      char character = sc.next().charAt(0);
+
+      System.out.println(character);
     }
 }
