3442fdf6d135a5716fab2dbbbc5c10bad87c304a
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 15:35:18 +0530

    Print Multiplication Table of Given Number:

--- Program.java
+++ Program.java
@@ -3,17 +3,14 @@ class Program
 {
     public static void main(String args[])
     {
-      //calculate the Number of digits in given Number:
+      //Print Multiplication Table of Given Number:
       
-      Scanner sc= new Scanner(System.in);
+      Scanner sc = new Scanner(System.in);
       System.out.print("Enter the Number: ");
       int Number = sc.nextInt();
 
-      int count = 0;
-      while(Number>0){
-        Number = Number/10;
-        count++;
+      for (int i = 1; i <=10; i++) {
+        System.out.println(Number+"*"+i+"="+Number*i);
       }
-      System.out.println("The Number "+Number+" is having "+count+" of digits!");
     }
 }
