82de1a7b26470b80d50a650d62811f8c01b39c45
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-07 00:00:42 +0530

    Swap Two Numbers without 3rd object:

--- Program.java
+++ Program.java
@@ -3,21 +3,21 @@ class Program
 {
     public static void main(String args[])
     {
-      //Swap Two Numbers:
+      //Swap Two Numbers without 3rd object:
       
-      int person1 = 10;
-      int person2 = 20;
-
-      int otherPerson;
+      int person1 = 20;
+      int person2 = 50;
 
       System.out.println("Person1 is having the money of "+person1);
       System.out.println("Person2 is having the money of "+person2);
 
       
       System.out.println("After the exchange or Swaping: ");
-      otherPerson = person1;
-      person1 = person2;
-      person2 = otherPerson;
+      
+      person1 = person1+person2; // 20+50=70;
+      
+      person2= person1 - person2;// 20
+      person1 = person1 - person2;//50
       
       System.out.println("Person1 is having the money of "+person1);
       System.out.println("Person2 is having the money of "+person2);
