31b7a8148f09866ca87d7661a07d84bc2f901568
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-07 02:35:14 +0530

    check the given Number is Palindrome or Not:

--- Program.java
+++ Program.java
@@ -3,18 +3,24 @@ class Program
 {
     public static void main(String args[])
     {
-      //Sum of all digits in the given Number:
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter the Number: ");
+      //check the given Number is Palindrome or Not:
+      Scanner sc=new Scanner(System.in);
+      System.out.print("Enter the Number that you want to check whether it is Palindrome: ");
 
       int Number = sc.nextInt();
+      
+      int temp= Number;
       int sum=0;
-
       while(Number>0){
         int rem = Number%10;
-        sum=sum+rem;
-        Number = Number/10;
+        sum=rem+(sum*10);
+        Number = Number/10;       
       }
-      System.out.print(sum);
+      if(sum==temp){
+        System.out.println("Number "+temp+" is Palindrome!");
+      }
+      else{
+        System.out.println("Number is NOT-Palindrome!");
+      }
     }
 }
