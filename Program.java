6b98961818aa77d099e38c2e999e235083dd633e
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 14:11:54 +0530

    check character is Vowel or Consosnant

--- Program.java
+++ Program.java
@@ -7,8 +7,13 @@ class Program
 
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter the Character: ");
-      char character = sc.next().charAt(0);
+      char character =  sc.next().charAt(0);
 
-      System.out.println(character);
+      if(character == 'A' || character == 'E' ||character == 'I' ||character == 'O' ||character == 'U'){
+        System.out.print(character+" is Vowel!");
+      }
+      else{
+        System.out.print(character+" is Consonant!");
+      }
     }
 }
