e5159557bc58d240098fa5001a326daabdfd5d82
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-06 14:20:49 +0530

    print ASCII value of given character

--- Program.java
+++ Program.java
@@ -3,17 +3,14 @@ class Program
 {
     public static void main(String args[])
     {
-      //print character from user input
+      //print ASCII value of given character 
 
       Scanner sc = new Scanner(System.in);
       System.out.print("Enter the Character: ");
-      char character =  sc.next().charAt(0);
 
-      if(character == 'A' || character == 'E' ||character == 'I' ||character == 'O' ||character == 'U'){
-        System.out.print(character+" is Vowel!");
-      }
-      else{
-        System.out.print(character+" is Consonant!");
-      }
+      char character = sc.next().charAt(0);
+
+      int value = character;
+      System.out.print(value);
     }
 }
