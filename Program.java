fedad9114f384bcc6645bc5d638b68e6d33f991b
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-07 00:33:17 +0530

    character to UpperCase and LowerCase

--- Program.java
+++ Program.java
@@ -3,23 +3,22 @@ class Program
 {
     public static void main(String args[])
     {
-      //Swap Two Numbers without 3rd object:
-      
-      int person1 = 20;
-      int person2 = 50;
+      //character to UpperCase and LowerCase:
 
-      System.out.println("Person1 is having the money of "+person1);
-      System.out.println("Person2 is having the money of "+person2);
+      Scanner sc = new Scanner(System.in);
+      System.out.print("Enter the character: ");
 
+      char character1 = sc.next().charAt(0);
+
+      char character2;
       
-      System.out.println("After the exchange or Swaping: ");
-      
-      person1 = person1+person2; // 20+50=70;
-      
-      person2= person1 - person2;// 20
-      person1 = person1 - person2;//50
-      
-      System.out.println("Person1 is having the money of "+person1);
-      System.out.println("Person2 is having the money of "+person2);
+      if(character1 >='A' && character1 <='Z'){
+        character2 = Character.toLowerCase(character1);
+        System.out.println(character2);
+      }else{
+        character2 = Character.toUpperCase(character1);
+        System.out.println(character2);
+      }
+    
     }
 }
