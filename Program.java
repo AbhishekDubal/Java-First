f00e6d46907dec7760d4ce6006680a9c67cbbc79
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-07 01:54:16 +0530

    Sum of all digits in the given Number

--- Program.java
+++ Program.java
@@ -3,16 +3,18 @@ class Program
 {
     public static void main(String args[])
     {
-      //Reverse the given Number:
+      //Sum of all digits in the given Number:
       Scanner sc = new Scanner(System.in);
       System.out.println("Enter the Number: ");
 
       int Number = sc.nextInt();
+      int sum=0;
 
       while(Number>0){
         int rem = Number%10;
-        System.out.print(rem);
-        Number=Number/10;
+        sum=sum+rem;
+        Number = Number/10;
       }
+      System.out.print(sum);
     }
 }
