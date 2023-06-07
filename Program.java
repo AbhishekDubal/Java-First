d515e2ea1ffa08099077f571effb96716bbc7692
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-07 19:30:51 +0530

    Armstrong Number(like 153)

--- Program.java
+++ Program.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
-class Program
-{
-    public static void main(String args[])
-    {
-      //check the given Number is Palindrome or Not:
-      Scanner sc=new Scanner(System.in);
-      System.out.print("Enter the Number that you want to check whether it is Palindrome: ");
+class Program {
+  public static void main(String[] args) {
+    //Armstrong Number(like 153):
 
-      int Number = sc.nextInt();
-      
-      int temp= Number;
-      int sum=0;
-      while(Number>0){
-        int rem = Number%10;
-        sum=rem+(sum*10);
-        Number = Number/10;       
-      }
-      if(sum==temp){
-        System.out.println("Number "+temp+" is Palindrome!");
-      }
-      else{
-        System.out.println("Number is NOT-Palindrome!");
-      }
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter the Number: ");
+    int Number = sc.nextInt();
+
+    int temp = Number;
+    int sum =0;
+
+    while(Number>0){
+      int rem = Number%10;
+      sum = sum+rem*rem*rem;
+      Number = Number/10;
     }
+
+    if(sum == temp){
+      System.out.println("The number "+temp+" is Palindrome!");
+    }
+    else{
+      System.out.println("The number "+temp+" is Non-Palindrome!");
+    }
+  }
 }
