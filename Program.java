e9896b567b1e16489569211ab13f704aaed3c8b9
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-08 11:00:56 +0530

    deepToString

--- Program.java
+++ Program.java
@@ -1,44 +1,9 @@
-import java.util.Scanner;
+import java.util.Arrays;
 class Program {
   public static void main(String[] args) {
-    //Arrays:- Array is a object that can store similar kind of elements in contagious memory(sequential manner).
-
-    //static array: lenth of array elemnts fixed.
-    int a[] = new int[4];
-    a[0]=10;
-    a[1]=108;
-    a[2]=105;
-    a[3]=170;
     
-    for (int i = 0; i < a.length; i++) {
-      System.out.print(a[i]+" ");
-    }
-
-    //dynamic array: Length will not be fixed.
-    int x[] = {2,3,45,5,35};
-    for (int i = 0; i < x.length; i++) {
-      System.out.print(x[i]+"   ");
-    }
-
-    //user input dynamic array at output:
-    Scanner sc =new Scanner(System.in);
-    System.out.print("Enter the length of the Array: ");
-    int length =sc.nextInt();
-
-    int array []= new int[length];
-
-    System.out.print("Enter the elements: ");
-    for (int i = 0; i < array.length; i++) {
-       array[i]=sc.nextInt();
-    }
-
-    for (int i = 0; i < array.length; i++) {
-      System.out.print(array[i]+" ");
-    }
-
-    System.out.println("By suing for each loop: ");
-    for (int i : array) {
-      System.out.print(i+" ");
-    }
+  //deepToString:
+  int rno[][] = {{1,2,3,45,5},{2,22,3,3,21,33,2}};  
+  System.out.println(Arrays.deepToString(rno));
   }
 }
