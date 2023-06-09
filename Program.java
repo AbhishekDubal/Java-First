247ce647cfedabaf8a989898f8e0ee781c7d5814
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-10 01:46:39 +0530

    Sort the array elements in Ascending order:

--- Program.java
+++ Program.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 class Program{
   public static void main(String[] args) {
-    //Search the element in the array:
+    //Sort the array elements in Ascending order:
 
     Scanner sc =new Scanner(System.in);
     System.out.println("Enter the size of array: ");
@@ -15,27 +15,25 @@ class Program{
       array[i] = sc.nextInt();
     }
 
-    System.out.print("The elements that you've entered: ");
+    System.out.println("The elements that you've entered: ");
 
     for (int i = 0; i < array.length; i++) {
       System.out.print(array[i]+" ");
     }
 
-    System.out.println("\nEnter the element that you want to search: ");
-    int search = sc.nextInt();
+    System.out.println("\nAfter Sorting the array elements in Ascending order: ");
 
-    int count=0;
     for (int i = 0; i < array.length; i++) {
-      if(search==array[i]){
-        count++;
+      for (int j = i+1; j < array.length; j++) {
+        if(array[i] > array[j]){
+          int temp= array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
       }
+      System.out.print(array[i]+" ");
+
     }
 
-    if (count>0) {
-      System.out.print("The "+search+ " element is found!");
-    }
-    else{
-      System.out.print("The "+search+ " element is NOT-found!");
-    }
   }
 }
