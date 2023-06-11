1be63421f4dabd96823c0e13cc492a16b83a5fdb
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-12 01:01:43 +0530

    left angle tri-angle patterns using java.

--- Program.java
+++ Program.java
@@ -1,9 +1,9 @@
 class Program{
   public static void main(String[] args) {
-    //patterns using java.
+    //left angle tri-angle patterns using java.
 
-    for (int i = 1; i < 5; i++) {
-      for (int j = 1; j < 5; j++) {
+    for (int i = 1; i <=5; i++) {
+      for (int j = 1; j <=i; j++) {
         System.out.print("* ");
       }
       System.out.println();
