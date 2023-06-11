873640bbcecd9e3c8ec05d804bdb7d89c6b867b3
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-12 00:58:59 +0530

    star printing using java

--- Program.java
+++ Program.java
@@ -1,31 +1,12 @@
-import java.util.Scanner;
 class Program{
   public static void main(String[] args) {
-    //Delete the element from the array:
+    //patterns using java.
 
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Enter the size of the Array: ");
-    int size = sc.nextInt();
-
-    int array[] = new int [size];
-
-    System.out.print("Enter the array elements in array: ");
-    for (int i = 0; i < size; i++) {
-      array[i]  = sc.nextInt();
+    for (int i = 1; i < 5; i++) {
+      for (int j = 1; j < 5; j++) {
+        System.out.print("* ");
+      }
+      System.out.println();
     }
-
-    System.out.println("The elements that you've eneterd now: ");
-    for (int i = 0; i < size; i++) {
-      System.out.print(" "+array[i]);
-    }
-
-    System.out.println("\nNow... What do you want to delete : ");
-    int location = sc.nextInt();
-    
-    for (int i = location; i < array.length -1; i++) {
-      array[i] = array[i+1];
-          System.out.print(array[i]+" ");
-    }
-
   }
 }
