aa83985648960aa1bf4ab0b838f0bd571d97380e
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-12 00:48:29 +0530

    Delete the element from the array:

--- Program.java
+++ Program.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 class Program{
   public static void main(String[] args) {
-    //Inserting another element in the array:
+    //Delete the element from the array:
 
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter the size of the Array: ");
     int size = sc.nextInt();
 
-    int array[] = new int [size+1];
+    int array[] = new int [size];
 
     System.out.print("Enter the array elements in array: ");
     for (int i = 0; i < size; i++) {
@@ -19,33 +19,13 @@ class Program{
       System.out.print(" "+array[i]);
     }
 
-    System.out.println("\nNow... Where do you want to insert your value: ");
+    System.out.println("\nNow... What do you want to delete : ");
     int location = sc.nextInt();
     
-    System.out.println("What value you want to enter there!");
-    int value = sc.nextInt();
-/**
- * //here it'll not give the last value. Because, suppose 4+1 =4, here if the 5th value is none in a sense the value would considered as 0(defalut integer value).
-    for (int i = 0; i < array.length; i++) {
-      if(location == i){
-        array[i+1] = array[i];
-      }
+    for (int i = location; i < array.length -1; i++) {
+      array[i] = array[i+1];
+          System.out.print(array[i]+" ");
     }
 
-    array[location] = value;
-    for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]+" ");
-    }
-   */
-  //So, to vaid this problem we should start with array's length
-
-  for(int i = array.length-1; i > location; i--){
-    array[i] = array[i-1];
-  }
-  array[location] = value;
-
-  for (int i = 0; i < array.length; i++) {
-    System.out.print(array[i]+" ");
-  }
   }
 }
