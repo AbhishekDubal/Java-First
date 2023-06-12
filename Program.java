b7970884113708c6ce89b150629784733470a426
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 02:09:31 +0530

    //Left UP side corner tri-angle patterns using java.

--- Program.java
+++ Program.java
@@ -1,12 +1,12 @@
 class Program{
   public static void main(String[] args) {
-    //Right UP side corner tri-angle patterns using java.
+    //Left UP side corner tri-angle patterns using java.
 
     for (int i = 1; i <=5; i++) {
       
-      for (int k = i; k > 1; k--) {
-        System.out.print(" ");
-      }
+      // for (int k = i; k > 1; k--) {
+      //   System.out.print(" ");
+      // }
       for (int j = 5; j >=i; j--) {
         System.out.print("*");
       }
