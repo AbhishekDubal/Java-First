9bab5d2fdb1b351ed654558bc7abdae883ec2344
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 14:07:48 +0530

    increase the value at each time where it prints

--- Program.java
+++ Program.java
@@ -1,10 +1,11 @@
 class Program{
   public static void main(String[] args) {
-    
+
+    int count =1;
+
     for(int i = 1; i<=5; i++){
-        int count =1;
       for(int j=1; j<=i;j++){
-        System.out.print(count);
+        System.out.print(count+" ");
         count++;
       }
       System.out.println();
