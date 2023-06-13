b3a7b14df31467a0756ed21cf78c9177e8d9497e
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 14:02:47 +0530

    increase the value at each column but start with the intiaal value 1 at each row

--- Program.java
+++ Program.java
@@ -2,8 +2,10 @@ class Program{
   public static void main(String[] args) {
     
     for(int i = 1; i<=5; i++){
+        int count =1;
       for(int j=1; j<=i;j++){
-        System.out.print(j);
+        System.out.print(count);
+        count++;
       }
       System.out.println();
     }
