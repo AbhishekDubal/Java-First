8bc459ad0a9442f47015be919cb4ac9eb2ca8f9a
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 15:45:23 +0530

    Fibonacci

--- Program.java
+++ Program.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
 class Program{
   public static void main(String[] args) {
-    char count = 'A';
-    for( char i = 'A' ; i<='E'; i++){
-      for(char j ='A'; j<=i;j++){
-        System.out.print(count);
-        count++;
-      }System.out.println();
+    //Fibonacci
+
+    int num1=0;
+    int num2=1;
+    int num3;
+
+    Scanner sc  = new Scanner(System.in);
+    System.out.println("Enter the range: ");
+    int range = sc.nextInt();
+
+      System.out.print(num1+" "+num2);
+    for(int i=1; i<= range;i++){     
+      num3= num1+num2;
+      System.out.print(" "+num3);
+      num1 = num2;
+      num2 = num3;
     }
   }
 }
