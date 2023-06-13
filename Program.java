15db3190be8ed079a0a8c2b87ddc3ae28cb1752e
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 13:56:23 +0530

    numbers at each rows and columns by increasing at each rows only

--- Program.java
+++ Program.java
@@ -2,8 +2,8 @@ class Program{
   public static void main(String[] args) {
     
     for(int i = 1; i<=5; i++){
-      for(int j=i; j>=1;j--){
-        System.out.print(i);
+      for(int j=1; j<=i;j++){
+        System.out.print(j);
       }
       System.out.println();
     }
