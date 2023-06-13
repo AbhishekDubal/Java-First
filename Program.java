5e1143c0201659dcfe691580011b3c022996e156
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 13:54:34 +0530

    print numbers at each rows an dcolumns

--- Program.java
+++ Program.java
@@ -2,12 +2,8 @@ class Program{
   public static void main(String[] args) {
     
     for(int i = 1; i<=5; i++){
-
-      for(int k = 5; k>i; k--){
-        System.out.print(" ");
-      }
-      for(int j=i; j>=1; j--){
-        System.out.print("* ");
+      for(int j=i; j>=1;j--){
+        System.out.print(i);
       }
       System.out.println();
     }
