a073a417cb0bdc390e0f4da0a2faa961e6f36409
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 13:42:12 +0530

    piramid triangle with out stars in between but little space in between

--- Program.java
+++ Program.java
@@ -6,8 +6,8 @@ class Program{
       for(int k = 5; k>i; k--){
         System.out.print(" ");
       }
-      for(int j=2*i-1; j>=1; j--){
-        System.out.print("*");
+      for(int j=i; j>=1; j--){
+        System.out.print("* ");
       }
       System.out.println();
     }
