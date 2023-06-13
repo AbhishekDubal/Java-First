a6b51835e2db241a913ad8fd447ab43146c6a384
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 14:31:50 +0530

    A BB CCC DDDD

--- Program.java
+++ Program.java
@@ -1,14 +1,9 @@
 class Program{
   public static void main(String[] args) {
-
-    int count =1;
-
-    for(int i = 1; i<=5; i++){
-      for(int j=1; j<=i;j++){
-        System.out.print(count+" ");
-        count++;
-      }
-      System.out.println();
+    for( char i = 'A' ; i<='E'; i++){
+      for(char j ='A'; j<=i;j++){
+        System.out.print(i);
+      }System.out.println();
     }
   }
 }
