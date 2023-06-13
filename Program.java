78ae5d63fb4f0a11f6cb1866bc8b2dec60b087e1
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 14:41:16 +0530

    A AB ABC

--- Program.java
+++ Program.java
@@ -2,7 +2,7 @@ class Program{
   public static void main(String[] args) {
     for( char i = 'A' ; i<='E'; i++){
       for(char j ='A'; j<=i;j++){
-        System.out.print(i);
+        System.out.print(j);
       }System.out.println();
     }
   }
