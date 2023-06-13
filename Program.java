f615bfc3f3a56df6cfcc587e2ade2910bbd75b03
AbhishekDubal <abhidubalgunde17@gmail.com>
2023-06-13 15:51:13 +0530

    Tribonacci

--- Program.java
+++ Program.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 class Program{
   public static void main(String[] args) {
-    //Fibonacci
+    //Tribonacci
 
     int num1=0;
     int num2=1;
-    int num3;
+    int num3 = 2;
+    int num4;
 
     Scanner sc  = new Scanner(System.in);
     System.out.println("Enter the range: ");
     int range = sc.nextInt();
 
-      System.out.print(num1+" "+num2);
-    for(int i=1; i<= range;i++){     
-      num3= num1+num2;
-      System.out.print(" "+num3);
+    System.out.print(num1 +" "+num2+" "+num3);
+
+    for(int i = 1; i<=range -4;i++){
+      num4 = num1+num2+num3;
+      System.out.print(" "+num4);
       num1 = num2;
       num2 = num3;
+      num3= num4;
     }
   }
 }
